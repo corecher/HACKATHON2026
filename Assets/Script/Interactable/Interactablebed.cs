4fbdc623c5d78160dbed05d559722b4bff2df6cd
kaiytr <iseungchan838@gmail.com>
2026-07-17 03:13:42 +0900

    RoomScene

---
+++ Assets/Script/Interactable/Interactablebed.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+public class InteractableBed : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject interactionUI;
+
+    private void Start()
+    {
+        if (interactionUI != null)
+        {
+            interactionUI.SetActive(false);
+        }
+    }
+
+    public void ShowUI(bool visible)
+    {
+        if (interactionUI != null)
+        {
+            interactionUI.SetActive(visible);
+        }
+    }
+
+    public void Interact()
+    {
+        
+    }
+}
