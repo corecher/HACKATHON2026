0fce243f4aa8edfe9e92c153393f09f87d244f4b
corecher <wooverjin@gmail.com>
2026-07-16 18:15:44 +0900

    StoreBasic

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -6,47 +6,31 @@ public enum GameState { Ready, Playing, Pause, GameOver }
 public class GameManager : Singleton<GameManager>
 {
     public GameState CurrentState { get; private set; } = GameState.Ready;
+    public int playerHp;
     
     // 해커톤 단골 데이터
-    public int Score { get; private set; }
-    public int BestScore { get; private set; }
+    public int score { get; private set; }
+    public int money { get; private set; }
     
     // 상태 변경 시 UI나 이펙트를 켜기 위한 이벤트
     public event Action<GameState> OnStateChanged;
-
+    
     void Start()
     {
-        BestScore = PlayerPrefs.GetInt("BestScore", 0);
+        score = PlayerPrefs.GetInt("BestScore", 0);
         ChangeState(GameState.Ready);
     }
 
     public void ChangeState(GameState newState)
     {
         CurrentState = newState;
-        
-        switch (newState)
-        {
-            case GameState.Ready: Time.timeScale = 1f; Score = 0; break;
-            case GameState.Playing: Time.timeScale = 1f; break;
-            case GameState.Pause: Time.timeScale = 0f; break;
-            case GameState.GameOver: Time.timeScale = 0f; CheckBestScore(); break;
-        }
-
         OnStateChanged?.Invoke(newState);
     }
 
     public void AddScore(int amount)
     {
         if (CurrentState != GameState.Playing) return;
-        Score += amount;
+        score += amount;
     }
 
-    private void CheckBestScore()
-    {
-        if (Score > BestScore)
-        {
-            BestScore = Score;
-            PlayerPrefs.SetInt("BestScore", BestScore);
-        }
-    }
 }
